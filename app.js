e00987cfcca341723224fee59a9e6d344709e8e7
hanthienduc <hanthienduc.96@gmail.com>
2021-02-01 14:09:02 +0900

    User Authentication Level 4 applying Salting and Hashing with bcrypt

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
 const app = express();
-
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 app.set("view engine", 'ejs')
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -36,34 +36,46 @@ app.get("/register", function(req, res) {
 });
 
 app.post("/register", function(req, res) {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
+    const password = req.body.password;
 
-    newUser.save(function(err) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("secrets");
+    bcrypt.hash(password, saltRounds, function(err, hash) {
+        // Store hash in your password DB.
+        if (!err) {
+            const newUser = new User({
+                email: req.body.username,
+                password: hash
+            });
+
+            newUser.save(function(err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.render("secrets");
+                }
+            });
         }
+
     });
 
 });
 
 app.post("/login", function(req, res) {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({ email: username }, function(err, foundUser) {
         if (err) {
             console.log(err);
         } else {
-            if (foundUser.password === password) {
-                res.render("secrets");
-            } else {
-                res.send("Password not matches!")
-            }
+            bcrypt.compare(password, foundUser.password, function(err, result) {
+                // result == true
+                if (result) {
+                    res.render("secrets");
+                } else {
+                    res.render("home");
+                }
+            });
+
         }
     });
 });
